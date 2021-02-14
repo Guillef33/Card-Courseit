174bfa6483211782bb2f40406ac4fc6711e63f5f
Guillef33 <guillef33@gmail.com>
2021-02-14 11:07:45 -0300

    Primeros Archivos Tarea 1 Clase 2

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn = document.getElementById('btn');
+const title = document.getElementsByClassName('title');
+
+
+btn.innerHTML = ('Hola Escribir')
+title.innerHTML = ('Hola Guillermo Flores')
+
+console.log(btn);
+console.log(title);
