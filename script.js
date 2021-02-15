4133568d23097b8f7a7fe03429487fd58752192e
Guillef33 <guillef33@gmail.com>
2021-02-15 08:50:44 -0300

    Apuntar ok a carpeta css

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const btn = document.getElementById('btn');
 const title = document.getElementsByClassName('title');
 const hr = document.getElementsByTagName('hr');
 
-btn.innerHTML = ('Cambio de texto en el boton')
-    
+btn.innerHTML = ('Cambio de texto')
+
 console.log(btn);
 console.log(title);
 console.log(hr)
